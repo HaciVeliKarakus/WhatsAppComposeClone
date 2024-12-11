c89db9c1064ff746d516aa401f9e33534f13350f
hvk <karakus.haciveli@gmail.com>
2024-12-11 03:18:49 +0300

    color added

---
+++ app/src/main/java/com/hvk/whatsapp/screen/HomeScreen.kt
@@ -0,0 +1,16 @@
+package com.hvk.whatsapp.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    HomeScreen()
+}
